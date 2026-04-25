e5bc5efda9f1c2e807301403613ac9de77131a9c
SamuelDevelop <ssoaresdossantos088@gmail.com>
2026-04-25 18:34:32 -0300

    inicio: Tiles e TileMap e Arquivos de tile

---
+++ myjavagame/src/main/java/com/myjavagame/tile/TileManager.java
@@ -0,0 +1,28 @@
+package com.myjavagame.tile;
+
+import java.awt.Graphics2D;
+
+import com.myjavagame.GamePanel;
+
+public class TileManager {
+    GamePanel gp;
+    Tile[] tile;
+
+    public TileManager(GamePanel gp) {
+        this.gp = gp;
+
+        tile = new Tile[10];
+    }
+
+    public void getTileImage(){
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public void draw(Graphics2D g2){
+        
+    }
+}
